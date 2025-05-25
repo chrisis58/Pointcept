6799188733ab41c640ea9cbeb4d2efa76abc0b98
chrisis <chrisis58@outlook.com>
2025-05-25 23:21:08 +0800

    fix: update checkoint save frequency

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -34,7 +34,7 @@ hooks = [
     dict(type="IterationTimer", warmup_iter=2),
     dict(type="InformationWriter"),
     dict(type="SemSegEvaluator"),
-    dict(type="CheckpointSaver", save_freq=None),
+    dict(type="CheckpointSaver", save_freq=1),
     dict(type="PreciseEvaluator", test_last=False),
 ]
 
