66a81ba2869bba7774149062c8755c06b38cae98
chrisis58 <chrisis58@outlook.com>
2025-05-27 22:43:17 +0800

    chore: specify checkpoint save  frequency

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -34,7 +34,7 @@ hooks = [
     dict(type="IterationTimer", warmup_iter=2),
     dict(type="InformationWriter"),
     dict(type="SemSegEvaluator"),
-    dict(type="CheckpointSaver", save_freq=1),
+    dict(type="CheckpointSaver", save_freq=5),
     dict(type="PreciseEvaluator", test_last=False),
 ]
 
